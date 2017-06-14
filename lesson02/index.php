af80dbfb7fa4bc38a84d7ff2029b4406244f2978
Gamzat Rasulov <gamzat_bandit3645@mail.ru>
2017-06-14 16:54:01 +0300

    Синтаксис PHP. Основы синтаксиса языка PHP

---
+++ lesson02/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Урок 1.</title>
+  </head>
+  <body>
+    <!-- Коментарий -->
+    <p>Привет мир!</p>
+
+    <?php
+
+    // Однострочный коментарий.
+    # это также однострочный  коментарий.
+    /*
+    Это
+    многострочный
+    коментарий
+    здесь не будут выполняться команды
+    echo "<h1>Hello World2!</h1>";
+    */
+    echo "<h1>Hello World2!</h1>";
+    echo "<h3>Hello World!</h3>";
+    print '<h1>Hello World2!</h1>';
+
+    ?>
+
+    <?php
+      echo "Привет 1".'<br>';
+      echo "Привет 4";
+
+    ?>
+    <?php echo "Привет 2"; ?>
+    <?php echo "Привет 3"; ?>
+
+
+  </body>
+</html>

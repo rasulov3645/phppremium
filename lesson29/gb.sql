2342abf0f85201543d91ee28d371514f43b1d6a5
Gamzat Rasulov <gamzat_bandit3645@mail.ru>
2017-06-28 18:47:29 +0300

    Сервер MySQL

---
+++ lesson29/gb.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июн 28 2017 г., 18:29
+-- Версия сервера: 5.5.53
+-- Версия PHP: 5.6.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `gb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `gb`
+--
+
+CREATE TABLE `gb` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `gb`
+--
+
+INSERT INTO `gb` (`id`, `name`, `text`, `date`) VALUES
+(1, 'Андрей', 'Привет!\r\nЭто сообщения раз', '2017-06-28 15:22:59'),
+(2, 'Вася', 'Hi!\r\nЭто сообщения два :)', '2017-06-28 15:26:52');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `gb`
+--
+ALTER TABLE `gb`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `gb`
+--
+ALTER TABLE `gb`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
